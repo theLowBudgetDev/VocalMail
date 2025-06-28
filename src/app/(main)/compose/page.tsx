d20a34135ce99205df743eeec7dbdbd62a80228e
Enrich Ugboji Onah <onahenrich@gmail.com>
2025-06-28 17:28:16 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(main)/compose/page.tsx
+++ src/app/(main)/compose/page.tsx
@@ -366,7 +366,7 @@ export default function ComposePage() {
               </form>
             </Form>
           </CardContent>
-        </card>
+        </Card>
       </div>
       <div className="fixed bottom-6 right-6 z-50">
         <TooltipProvider delayDuration={100}>
